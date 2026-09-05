dfbae4d2e871b25aeb55e710e59136725e90ebeb
Efrain Bautista <efrain@thibau.io>
2026-09-05 07:24:56 -0500

    chore: initial commit.

---
+++ Acme.Hello.Platform/Program.cs
@@ -0,0 +1,25 @@
+using Scalar.AspNetCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.MapScalarApiReference(options =>
+    {
+        options.WithTitle("Hello ASP.NET Developer API")
+            .WithTheme(ScalarTheme.DeepSpace)
+            .WithDefaultHttpClient(ScalarTarget.CSharp, ScalarClient.HttpClient);
+    });
+}
+
+app.UseHttpsRedirection();
+
+app.Run();
